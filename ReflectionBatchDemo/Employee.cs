1e02fc52e68dccdbe6dcec44fa9d008d602ba9c6
Snehal979 <bansodsnehal535@gmail.com>
2023-02-24 13:51:09 +0530

    [Snehal]Add: create variable and property,Method and Constructor

---
+++ ReflectionBatchDemo/Employee.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ReflectionBatchDemo
+{
+    public class Employee
+    {
+        //Variable
+        int idNo;
+        string name;
+        //Property
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public Employee() // //Default constructor
+        {
+            Console.WriteLine("Default constructor");
+            
+        }
+        public Employee(int Id) //Parametric constructor
+        {
+            this.Id =Id;
+        }
+        //Method
+        public void Method1()
+        {
+            Console.WriteLine("id "+idNo);
+            Console.WriteLine("Name "+name);
+        }
+        public int Method2()
+        {
+            int a = 80;
+            return a;
+        }
+        public string Method3(string deperatment)
+        {
+            return deperatment;
+        }
+    }
+}
